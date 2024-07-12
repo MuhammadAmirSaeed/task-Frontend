b59a98648da80a4692d20a2158e1fa9d4d2f1d16
MuhammadAmirSaeed <amirsaeed.gcuf@gmail.com>
2024-07-13 03:02:15 +0500

    work

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 import { getCookie } from '../utils/cookiesConfig'; 
 const token = getCookie('token');
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Base URL for your API requests
+  // baseURL: "http://localhost:8000", 
+  baseURL: "https://task-backend-e-commerce-h77k68dnw-amir-saeeds-projects.vercel.app/", // Base URL for your API requests
+  
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
