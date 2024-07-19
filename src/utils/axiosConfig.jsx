a18ac5b5943de2e969631d2d9daffabee90f40ac
MuhammadAmirSaeed <amirsaeed.gcuf@gmail.com>
2024-07-19 18:22:25 +0500

    work

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   // baseURL: "http://localhost:8000", 
   // baseURL: "https://task-backend-e-commerce.vercel.app",
   baseURL: "  https://task-backend-gwlh3kbjk-amir-saeeds-projects.vercel.app/ ",// Base URL for your API requests
-  
+  withCredentials:true,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
